4c3d54df1b28aa3edb4c5a9da778a9b8c97db753
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2024-04-22 14:08:08 +0400

    1

---
+++ Assets/CodeBase/DestroyAnyColorGameMode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DestroyAnyColorGameMode : GameMode
+{
+    private BallColors _targetColor;
+
+    public DestroyAnyColorGameMode(string target) : base(target)
+    {
+    }
+
+    public override void Init(IReadOnlyCollection<Ball> balls)
+    {
+        var allColors = Enum.GetValues(typeof(BallColors));
+        _targetColor = (BallColors)allColors.GetValue(UnityEngine.Random.Range(0, allColors.Length));
+        BallsLeft = balls.Where(i => i.Data.Color == _targetColor).Count();
+
+        Target = $"Уничтожить все шары {_targetColor} цвета";
+    }
+
+    protected override void OnBallDestroyed(BallData destroyedBallData)
+    {
+        if (destroyedBallData.Color != _targetColor)
+        {
+            OnTargetFailed();
+            return;
+        }
+
+        BallsLeft--;
+
+        if (BallsLeft == 0)
+            OnTargetReached();
+    }
+}
